1bc6df798940cc847737a75accc2bd440d98efcf
Julian Janicki <janickijuliann@gmail.com>
2023-12-13 23:07:33 +0100

    Automatyczne włączanie SSL w aplikacji.

---
+++ aplikacja/sql_tools/init_ssl.sql
@@ -0,0 +1,3 @@
+UPDATE `ps_configuration` SET `value` = '1' WHERE `name` = 'PS_SSL_ENABLED';
+UPDATE `ps_configuration` SET `value` = 'localhost' WHERE `name` = 'PS_SHOP_DOMAIN';
+UPDATE `ps_configuration` SET `value` = 'localhost' WHERE `name` = 'PS_SHOP_DOMAIN_SSL';
