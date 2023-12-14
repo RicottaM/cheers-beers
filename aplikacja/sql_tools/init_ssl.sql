bc666091d57fa17a17a29e1771a4eabdd9100042
Julian Janicki <janickijuliann@gmail.com>
2023-12-14 02:05:35 +0100

    Usuniecie automatycznego backupu.

--- aplikacja/sql_tools/init_ssl.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `ps_configuration` SET `value` = '1' WHERE `name` = 'PS_SSL_ENABLED';
-UPDATE `ps_configuration` SET `value` = 'localhost' WHERE `name` = 'PS_SHOP_DOMAIN';
-UPDATE `ps_configuration` SET `value` = 'localhost' WHERE `name` = 'PS_SHOP_DOMAIN_SSL';
---
+++ aplikacja/sql_tools/restore.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -h mysql -u root -pprestashop prestashop < /var/lib/mysql/db_backup/backup.sql
